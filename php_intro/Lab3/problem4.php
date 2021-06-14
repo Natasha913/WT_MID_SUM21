99d55bc925b723f47269a5be24c7cf6540d20e1c
natasha <natashakainat91@gmail.com>
2021-06-14 11:00:50 +0600

    lab3

---
+++ php_intro/Lab3/problem4.php
@@ -0,0 +1,17 @@
+<html>
+<body>
+<?php
+     $name = "mithu";
+	 if
+	
+		 ($name == 'raju' or  $name == 'mina' or $name == 'mithu')
+	{
+	 echo "Hallo  "  .$name.  "<br>";
+	 }
+	 else
+	 {
+		echo "you are not recognized";
+	 }
+?>
+</body>
+</html>
