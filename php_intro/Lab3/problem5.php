fdcad186f2ccadc760abad151ad05eb3e7a17292
natasha <natashakainat91@gmail.com>
2021-06-14 20:45:25 +0600

    lab3

---
+++ php_intro/Lab3/problem5.php
@@ -0,0 +1,15 @@
+<html>
+      <body>
+	        <?php
+			$blackColor="green";$frontColor="red";$imageWidth="80px";$imageHeight="80px";
+			?>
+			<table>
+			<tr>
+			<td><img src = "aiub_logo.jpg" width = "180px" height="180px"/></td>
+			<td><h2>American International University-<span style = "background-color:green;color:red">
+			Bangladesh</h2></td>
+			</tr>
+			</span>
+			</table>
+	  </body>
+</html>
